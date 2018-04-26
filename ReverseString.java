c62aec37ad0d854e45f4c982906aea74a789523a
ratankhalsa92 <ratan.khalsa92@gmail.com>
2018-04-25 23:49:09 -0400

    LeetCode solution - Reversing a string

---
+++ ReverseString.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ReverseString {
+
+    public static String reverseString(String str1) {
+        StringBuffer sbf = new StringBuffer();
+
+        for(int i=str1.length()-1; i>=0; i--) {
+            sbf.append(str1.charAt(i));
+        }
+
+        return sbf.toString();
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter a String:");
+
+        Scanner sc = new Scanner(System.in);
+//        String str1 = new String();        // Why?
+        String str1 = sc.nextLine();
+
+        String result = reverseString(str1);
+        System.out.println(result);
+    }
+}
